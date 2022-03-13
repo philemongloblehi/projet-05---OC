5344cfd30838ed93d81e8311bea2c24853fc9a00
kilco <jacques61593@gmail.com>
2022-03-13 15:33:42 +0100

    cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,9 +1,32 @@
 panier = JSON.parse(localStorage.getItem("produit"));
 
-const elementPanier = document.querySelector('cart__items');
+let elementPanier = document.querySelector("cart__item");
 
-if(panier === null){            // si le panier est vide
-  console.log("je suis vide");
-}else{                          // sinon
-  elementPanier.localStorage
+if (panier === null) {
+  // si le panier est vide
+  let panierVide;
+  elementPanier.innerHTML = panierVide;
+} else {
+  // sinon
+  // produit a afficher
+  console.log("je suis pas vide");
+  let panierAfficher = [];
+
+  for (i = 0; i < panier.length; i++) {
+    panierAfficher =
+      panierAfficher +
+      `
+    <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+                <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                    <h2>${panier[i].name}</h2>
+                    <p>${panier[i].quantity}</p>
+                    <p>${panier[i].price}€</p>
+                  </div>
+    `;
+  }
+  if (i === panier.length);
+  {
+    elementPanier.innerHTML = panierAfficher;
+  }
 }
