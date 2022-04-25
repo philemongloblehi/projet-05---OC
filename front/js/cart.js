18b40f0a07c71ae570ecef6e489c353dc3b193de
Philemon GLOBLEHI <philemon.globlehi@gmail.com>
2022-04-25 22:26:44 +0200

    Commit

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,49 +1,39 @@
-let productId = new URL(window.location.href).searchParams.get('id')
-panier = JSON.parse(localStorage.getItem("produit"));                     // on recupere les donner dans le localstorage
-  let product;                                                            // on declare une variable product
-const getProduct = async () => {                                        // on cree une constante pour obtenir les produits sur le site
-  await fetch('http://localhost:3000/api/products/')                    // ajout des produits sur le site grace a l'api avec fetch 
-  .then(res => res.json())                                              // reponse
-  .then(JSON => product = JSON)                                          
-  .catch((err) => console.log(err));                                    // pour afficher les erreurs si il y a des erreurs
-  console.log(product) 
-}
+let productId = new URL(window.location.href).searchParams.get("id");
+let panier = JSON.parse(localStorage.getItem("produit"));                                                         // on recupere les donner dans le localstorage
+let products;                                                                                                 // on declare une variable product
+let product;
+let prixTotal = 0;
+let prixProduit = 0;
+let panierAfficher = [];
+const getProduct = async () => {                                                                              // on cree une constante pour obtenir les produits sur le site
+    await fetch("http://localhost:3000/api/products/")                                                        // ajout des produits sur le site avec l'api en utilisant fetch
+        .then((res) => res.json())                                                                                // reponse
+        .then((JSON) => (products = JSON))                                                                        // on demande que les produits sont afficher en format json
+        .catch((err) => console.log(err));                                                                        // pour afficher les erreurs si il y a des erreurs
+    console.log(products);                                                                                      // on demande a afficher les produits dans la console
 
-getProduct();
-const showProduct =  () => {            
-   getProduct();                                              // en attente du produit                   
-  let picture = document.querySelector("cart__item__img")               // nous utilisons le DOM pour recuperer un element dans le html puis pour le donner une nouvelle variable
-  picture.setAttribute('src', product.imageUrl)                   // nous mettons a jour l'attribut src par product.imageUrl
-  pictureAlt.setAttribute('alt', product.altTxt)                     // nous mettons a jour l'attribut alt par product.altTxt
-  picture.appendChild(imageUrl)                                    // nous disons que picture est l'enfant de imageUrl
-
-  let name = document.getElementById('cart__item__content__description')                      // nous donnons a title une nouvelle variable 
-  name.innerHTML = product.name;                                   
-
-  let price = document.getElementById('cart__item__content')                     // nous donnons une nouvelle variable a price (ou pas)
-  price.innerHTML = product.price;
-  
-} 
-
-const elementPanier = document.querySelector("#cart__items");                     // on cree une variable element panier 
-if (panier === null) {                                                             // si le panier est vide                             
-  let panierVide;                                                                  // on declare une nouvelle variable
-  elementPanier.innerHTML = panierVide;                                           // on definit element panier = parent de panier vide
-} else {                                                                          // sinon
-   let panierAfficher = [];                                                      // on cree un tableau vide pour la page panier
-   for (i = 0; i < panier.length; i++) {                                         // on cree une boucle qui sera repeter pour chaque nouveau element
-    panierAfficher =
-      panierAfficher +
-      `
+    const elementPanier = document.querySelector("#cart__items");                      // on cree une variable element panier puis on selectionne la div contenant la class cart items
+    if (panier === null) {                                                             // si le panier est vide
+        let panierVide;                                                                  // on declare une nouvelle variable pour le panier vide
+        elementPanier.innerHTML = panierVide;                                            // on definit element panier = parent de panier vide
+    } else {                                                                           // sinon
+        panierAfficher = [];                                                             // on cree un nouveau tableau pour afficher le panier sur la page
+        for (i = 0; i < panier.length; i++) {                                            // on cree une boucle qui sera repeter pour chaque nouveau element
+            product = products.filter(prod => prod._id === panier[i].id);
+            prixProduit = product[0].price;
+            prixTotal = prixTotal + prixProduit;
+            panierAfficher =                                                            // on dit que panier afficher = le tableau + le html avec des element pris dans l'api + localstorage
+                panierAfficher +
+                `
       <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
       <div class="cart__item__img">
-        <img src="${panier[i].imageUrl}" alt="Photographie d'un canapé">
+        <img src="${product[0].imageUrl}" alt="Photographie d'un canapé">
       </div>
       <div class="cart__item__content">
         <div class="cart__item__content__description">
-          <h2>${panier[i].name}</h2>
+          <h2>${product[0].name}</h2>
           <p>${panier[i].color}</p>
-          <p>${panier[i].price} €</p>
+          <p>${prixProduit} €</p>
         </div>
         <div class="cart__item__content__settings">
           <div class="cart__item__content__settings__quantity">
@@ -57,14 +47,36 @@ if (panier === null) {                                                          
       </div>
     </article>
     `;
-  }
-  if (i === panier.length)
-  {
-    elementPanier.innerHTML = panierAfficher
-  }
-}
+        }
+        console.log("Prix total produit", prixTotal);
+        if (i === panier.length) {                                                        // on ajoute les elements
+            elementPanier.innerHTML = panierAfficher;
+        }
 
 
-// modifier quantiter
-let itemQuantity = document.querySelector("itemQuantity");
+        let supprimer = document.querySelectorAll(".deleteItem");                         // on declare une variable pour supprimer en selectionner la class du bouton supprimer
+        console.log(supprimer);                                                           // on verifie si la variable et bien lier au bouton supprimer
+        for (let q = 0; q < supprimer.length; q++){                                       // on cree une boucle
+            supprimer[q].addEventListener("click", (event) =>{                             // on va mettre un add event listener pour le bouton supprimer
+                event.preventDefault();                                                       // pour ne pas recharger la page
+                let elementSupprimer = panier[q].id;                                          // on cree une variable qui sera supprimer en utilisant l'id
+                let monFilter = {
+                    id : panier[q].id,
+                    color : panier[q].color
+                };
+                panier= panier.filter(function(item) {
+                    for (var key in monFilter) {
+                        if (item[key] != monFilter[key])
+                            return true;
+                    }
+                    return false;
+                });
+                // panier = panier.filter(element => element.id !== elementSupprimer)            // on utilise la methode filter pour retirer l'element ou j'ai appuyer sur supprimer
+                localStorage.setItem("produit" ,JSON.stringify(panier));                      // on met a jour la clee
+                window.location.href = "cart.html";                                           // on actualise la page
+            });
+        }
+    }
+};
 
+getProduct();
